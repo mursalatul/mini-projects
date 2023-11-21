f891034557e74fd0ebc72058bf5110a82762025b
mursalatul <mursalatulislampallob.com>
2023-11-22 03:54:22 +0600

    core functionality added

--- gnu_sound_enhancer/control.py
+++ gnu_sound_enhancer/control.py
@@ -8,11 +8,3 @@ def volume(vol: str) -> bool:
     # running the command
     status = subprocess.run(volume_command).returncode
     return not status # returncode = 0 when successfully volume changed, else 1
-
-def main():
-    for i in range(5):
-        vol = input("Volume Change: ")
-        print(volume(vol))
-
-if __name__ == "__main__":
-    main()
--- gnu_sound_enhancer/gui.py
+++ gnu_sound_enhancer/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from control import volume
 class GUI:
     # store app instance
     app = None
@@ -13,27 +13,26 @@ class GUI:
         self.app.geometry('350x200')
 
         # scale
-        self.slider = tk.Scale(self.app, from_=0, to=100, orient=tk.HORIZONTAL)
+        self.slider = tk.Scale(self.app, from_=0, to=200, orient=tk.HORIZONTAL, command=self.changeVolume)
         self.slider.pack()
         
         # creating frame to store the 3 buttons
         self.three_button_frame = tk.Frame(self.app,  bd=5, relief=tk.GROOVE, highlightbackground="red", highlightcolor="red")
         self.three_button_frame.pack( )
 
-        # plus button
-        self.plus_button = tk.Button(self.three_button_frame, text="+")
-        self.plus_button.pack(side=tk.LEFT)
-
-        # minus button
-        self.minus_button = tk.Button(self.three_button_frame, text="-")
-        self.minus_button.pack(side=tk.LEFT)
-
         # exit button
-        self.minus_button = tk.Button(self.three_button_frame, text= 'X')
-        self.minus_button.pack(side=tk.LEFT)
-    
-    # def functions(self):
+        self.exit_button = tk.Button(self.three_button_frame, text= 'X', command=self.exitProgram)
+        self.exit_button.pack(side=tk.LEFT)
 
+
+    # increase volume 
+    def changeVolume(self, vol):
+        status = volume(str(100+int(vol)))
+
+    # exit by setting the volume system 50%
+    def exitProgram(self):
+        status = volume("50")
+        self.app.destroy()
 
 def main():
     app = tk.Tk()

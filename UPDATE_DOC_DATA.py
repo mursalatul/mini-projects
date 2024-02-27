ca6254a1bccef4f090935b07c1039ce60fec7102
mursalatul <mursalatulislampallob@gmail.com>
2024-02-28 04:02:01 +0600

    update values for mini-projects

--- UPDATE_DOC_DATA.py
+++ UPDATE_DOC_DATA.py
@@ -1,10 +1,10 @@
 # change the following data according to your need
 content = {
-    'title' : "documentation title",
+    'title' : "Mini Projects",
     'sub_title': "Content",
-    'text' : 'this is a usual text to show after title or sub title',
+    'text' : 'Collection of my simple projects. This projects are made for practice purpose. Nothing serious here',
     'show_file' : True,
     'show_folder' : True,
-    'ignore' : ['.github', '.git', 'UPDATE_DOC.py', 'UPDATE_DOC_DATA.py', 'LICENSE', 'virtual_env', '__pycache__', 'DOCUMENTATION.md', 'README.md', 'run_doc_gen_script.yml'],
-    'path' : "https://github.com/mursalatul/github-doc-gen/"
+    'ignore' : ['.gitignore','.github', '.git', 'UPDATE_DOC.py', 'UPDATE_DOC_DATA.py', 'LICENSE', 'virtual_env', '__pycache__', 'DOCUMENTATION.md', 'README.md', 'run_doc_gen_script.yml'],
+    'path' : "https://github.com/mursalatul/mini-projects/"
 }
